236aca984a0691a08db7744d573746242ae5604c
Kamil Jakóbczak <k_j1@vp.pl>
2023-02-13 12:22:25 +0100

    handle missing files for covers

--- src/apollo/imagesRouter.ts
+++ src/apollo/imagesRouter.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import path from 'path';
 import { coverResize } from '../utility/coverResize';
 import fs from 'fs';
+import sharp from 'sharp';
+
 export const imagesRouter = express.Router();
 const imagesDir = path.join(__dirname, '..', '..', 'images');
 
@@ -17,25 +19,51 @@ imagesRouter.get('/covers/:bookId/:size', async (req, res) => {
   const { bookId, size } = req.params;
   const coversPath = path.join(imagesDir, 'covers');
 
-  const filePath = path.join(coversPath, bookId, `${size}.jpg`);
+  const bookDir = path.join(coversPath, bookId);
+  const bookDirExists = fs.existsSync(bookDir);
+  if (!bookDirExists) {
+    res.send('no covers avaiable for this one');
+    res.end();
+  }
+
+  const filePath = path.join(bookDir, `${size}.jpg`);
 
   const fileExists = fs.existsSync(filePath);
 
+  const originalPath = path.join(coversPath, bookId, 'original.jpg');
+  const originalExists = fs.existsSync(originalPath);
+  const originalHeight =
+    originalExists && (await sharp(originalPath).metadata()).height;
+
   const mediumPath = path.join(coversPath, bookId, 'medium.jpg');
   const mediumExists = fs.existsSync(mediumPath);
+  const mediumHeight =
+    mediumExists && (await sharp(mediumPath).metadata()).height;
+
   const smallPath = path.join(coversPath, bookId, 'small.jpg');
   const smallExists = fs.existsSync(smallPath);
+  const smallHeight = smallExists && (await sharp(smallPath).metadata()).height;
 
   if (fileExists) {
     res.sendFile(filePath);
-  } else if (size === 'big' && mediumExists) {
-    res.sendFile(mediumPath);
-  } else if (size === 'medium' && smallExists) {
-    res.sendFile(smallPath);
+  } else if (size === 'big') {
+    if (originalHeight && mediumHeight) {
+      if (originalHeight > mediumHeight) {
+        res.sendFile(originalPath);
+      } else res.sendFile(mediumPath);
+    }
+  } else if (size === 'medium') {
+    if (originalHeight && smallHeight) {
+      if (originalHeight > smallHeight) {
+        res.send(originalPath);
+      } else {
+        res.send(smallPath);
+      }
+    }
   } else {
     console.log('Something went wrong with preparing the cover for this one');
     res.send('Something went wrong with preparing the cover for this one');
   }
 
-  coverResize(path.join(coversPath, bookId, 'original.jpg'));
+  // coverResize(path.join(coversPath, bookId, 'original.jpg'));
 });
